3518d66ab6825e3ef136a14e1bf53ee61ef1af22
Loic Pfister <pfister.loic@gmail.com>
2021-04-07 08:16:53 +0200

    no message

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import Link from '@material-ui/core/Link';
-import ProTip from './ProTip';
 import BuyCaluculatorForm from './BuyCaluculatorForm';
 
 
